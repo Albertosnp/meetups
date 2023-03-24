8beeced64afe8a0969a73c2bd8344da7093f2913
Alberto <albertosnp91@gmail.com>
2023-03-24 18:15:42 +0100

    feat: Add header scroll

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,21 @@
-import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/constants";
+import { useHeaderScroll } from '../../util-hooks/useHeaderScroll';
+import {
+  ALL_MEETUP_PAGE,
+  FAVORITES_PAGE,
+  NEW_MEETUP_PAGE,
+} from './../../utils/constants';
 
-import classes from "./MainNavigation.module.css";
+import classes from './MainNavigation.module.css';
 
 export default function MainNavigation({ setPage }) {
+  const { scrollDirection } = useHeaderScroll();
+  const extraClass = scrollDirection === 'down' ? classes.sticky : '';
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header
+      className={`${classes.header} ${extraClass}`}
+      data-test="navigation-header"
+    >
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
